a3f286b171f808a37f37aa203c0643b9d5ae4bd5
bella-stack <80238453+bella-stack@users.noreply.github.com>
2022-01-26 21:14:05 -0600

    increase memory

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a929b9eb6363d480fac444eeb953de5c-1187585629.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
